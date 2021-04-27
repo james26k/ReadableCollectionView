2f6ded1a51593dd10f2ae0bd4327c361c8de930b
james26k <cb26.hayashi@gmail.com>
2021-04-28 01:34:39 +0900

    add: CollectionViewのレイアウトを作成

--- ReadableCollectionView/InnusModule/InnusViewController.swift
+++ ReadableCollectionView/InnusModule/InnusViewController.swift
@@ -7,7 +7,12 @@
 
 import UIKit
 
-class InnusViewController: UIViewController {
+final class InnusViewController: UIViewController {
+    private let collectionView = UICollectionView(frame: .zero,
+                                                  collectionViewLayout: UICollectionViewFlowLayout())
+
+    private let cellSpacing: CGFloat = 10
+
     override func loadView() {
         super.loadView()
         view.backgroundColor = .systemBackground
@@ -15,5 +20,58 @@ class InnusViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.backgroundColor = .secondarySystemBackground
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
+
+extension InnusViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        30
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .gray
+        return cell
+    }
+}
+
+extension InnusViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellSideLength: CGFloat = (view.readableContentGuide.layoutFrame.width - cellSpacing * 2) / 3
+        return .init(width: cellSideLength, height: cellSideLength)
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        let sectionSideMargin: CGFloat = (view.frame.width - view.readableContentGuide.layoutFrame.width) / 2
+        return .init(top: cellSpacing, left: sectionSideMargin,
+                     bottom: cellSpacing, right: sectionSideMargin)
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        cellSpacing
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        cellSpacing
     }
 }
