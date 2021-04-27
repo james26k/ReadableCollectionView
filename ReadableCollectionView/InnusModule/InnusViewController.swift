766adaa96b6a8520c1c62deb345ac010f6168e53
james26k <cb26.hayashi@gmail.com>
2021-04-27 23:00:33 +0900

    remove: SceneDelegateとMain.storyboardを削除してコードで初めに表示するViewControllerを設定

---
+++ ReadableCollectionView/InnusModule/InnusViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InnusViewController.swift
+//  ReadableCollectionView
+//
+//  Created by Kohei Hayashi on 2021/04/27.
+//
+
+import UIKit
+
+class InnusViewController: UIViewController {
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .systemBackground
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
